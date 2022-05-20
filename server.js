14d1b48aa58cf87548222eb16d968537664d944a
Avyansh Ralph <me@avyansh.codes>
2022-05-20 14:37:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "index.html");
 });
+
+app.get("/avi", (request, response) => {
+  response.sendFile(__dirname + "https://avyansh.codes");
+});
+
 app.get("/*", (request, response) => {
   response.sendFile(__dirname + "404.html");
 });
